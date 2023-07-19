e866c4b48425a666727109c56df74c196d241bb1
abidueiro <abidueiro@pop-os.localdomain>
2023-07-19 13:37:06 +0200

    lint calc.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,14 @@
+
+
 """
 Calculator fucntions
 """
 def add(x, y):
-    
+
     """Add x and y and return the result."""
     return x + y 
+
 def subtract(x, y):
-    
+
     """Subtract x and y and return the result."""
     return y - x
