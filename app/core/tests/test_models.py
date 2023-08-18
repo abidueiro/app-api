69c86a0905c8ea49435eb71f143aa62eb4ead2da
abidueiro <berzas@riseup.net>
2023-08-18 11:12:48 +0200

    Add superuser suport to custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,20 @@ class ModelTests(TestCase):
         for email, expeted in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expeted)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+
