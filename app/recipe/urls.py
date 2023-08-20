2e8ec7abb7f32c32169e5b9e33952b43fbdebceb
abidueiro <berzas@riseup.net>
2023-08-20 10:49:48 +0200

    Implement recipe listing API

---
+++ app/recipe/urls.py
@@ -0,0 +1,21 @@
+"""
+Url mappings for the recipe API.
+"""
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+router = DefaultRouter()
+router.register(r'recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
