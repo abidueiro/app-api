80aa51bb8b8ee6249e0a4bdb39073ceabe6a48c1
abidueiro <berzas@riseup.net>
2023-08-20 11:56:38 +0200

    Implement create recipe API

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -107,7 +107,7 @@ class PrivateRecipeAPITests(TestCase):
             'title': 'Sample recipe',
             'time_minutes': 30,
             'price': Decimal('5.99'),
-            }
+        }
         res = self.client.post(RECIPES_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
